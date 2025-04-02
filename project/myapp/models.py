257b5da4494873e4ad3b719d28b39d16c2d06356
akhoa6204 <khoaanh662004@gmail.com>
2025-04-02 14:29:31 +0700

    update database

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,3 +1,72 @@
+from telnetlib import CHARSET
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    phone=models.CharField(max_length=50)
+    address=models.TextField()
+
+class Category(models.Model):
+    name=models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Brand(models.Model):
+    name=models.CharField(max_length=150)
+    nation=models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    name=models.CharField(max_length=150)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category =models.ForeignKey(Category, on_delete=models.CASCADE)
+    purchased_count = models.PositiveIntegerField(default=0)
+    description = models.TextField(null=True, blank=True)  # Mô tả sản phẩm chi tiết
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Option(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.CharField(max_length=150, null=True, blank=True)  # Màu sắc
+    ram = models.CharField(max_length=150, null=True, blank=True)  # Dung lượng RAM
+    rom = models.CharField(max_length=150, null=True, blank=True)  # Dung lượng ROM
+    screen_size = models.CharField(max_length=50, null=True, blank=True)  # Kích thước màn hình
+    battery = models.CharField(max_length=50, null=True, blank=True)  # Dung lượng pin
+    chipset = models.CharField(max_length=150, null=True, blank=True)  # Chip xử lý
+    camera = models.CharField(max_length=150, null=True, blank=True)  # Thông số camera
+    os = models.CharField(max_length=150, null=True, blank=True)  # Hệ điều hành
+    charging_type = models.CharField(max_length=150, null=True, blank=True)  # Loại cáp sạc (VD: "USB-C", "Lightning")
+    warranty = models.CharField(max_length=150, null=True, blank=True)  # Bảo hành (VD: "12 tháng", "24 tháng")
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # Giá tùy chọn
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Cart(models.Model):
+    option =models.ForeignKey(Option, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def total_price(self):
+        return self.option.price * self.quantity
+
+class Purchased(models.Model):
+    option =models.ForeignKey(Option, on_delete=models.DO_NOTHING)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    date_purchased=models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    description=models.TextField()
+    star_count = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# class Payment(models.Model):
+
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'project.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +73,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+AUTH_USER_MODEL = 'myapp.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -114,8 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
