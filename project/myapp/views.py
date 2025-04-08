0fcbb674b882c2360a6585c746ccf0f8d8832ccf
akhoa6204 <khoaanh662004@gmail.com>
2025-04-08 14:48:33 +0700

    update html

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'components/components/header.html')
+    return render(request, 'page/home.html')
